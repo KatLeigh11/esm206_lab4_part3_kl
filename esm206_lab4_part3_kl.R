db78a7273f2dc5182bd5ab102a82a38c3b3c95a8
KatLeigh11 <kleigh@ucsb.edu>
2019-10-21 14:00:30 -0700

    create graphs

--- esm206_lab4_part3_kl.R
+++ esm206_lab4_part3_kl.R
@@ -11,3 +11,14 @@ library(here)
 db <- read_csv(here::here("data","disease_burden.csv")) %>% 
   clean_names() %>% 
   rename(deaths_per_100k = death_rate_per_100_000)
+
+#create a subset, then graph:
+
+sb_sub <- db %>% 
+  filter(country_name %in% c("United States", "Japan", "Afghanistan", "Somalia")) %>% 
+  filter(age_group == "0-6 days", sex == "Both")
+
+#graphing (line)
+
+ggplot(data = sb_sub, aes(x = year, y = deaths_per_100k))+
+  geom_line(aes(color = country_name))
